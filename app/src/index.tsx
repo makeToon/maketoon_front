84f2dd90f341970870b466fda1f9d41cfe31adbb
jjunu <junukim.dev@gmail.com>
2020-01-28 15:10:28 +0900

    [CREATE & UPDATE] container/auth UseAuthRedux custom hook

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,12 +2,12 @@ import * as React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 
-// import { store } from "data/store";
+import { store } from "data/store";
 import App from "./page";
 
 render(
-  // <Provider store={store}>
-  <App />,
-  // </Provider>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
