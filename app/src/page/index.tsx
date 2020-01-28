4f2b8b09eb07edffcb8cabd7dca6467148d3dd0c
jjunu <junukim.dev@gmail.com>
2020-01-28 18:55:54 +0900

    [MODIFY] page/index.tsx remove token save function

--- app/src/page/index.tsx
+++ app/src/page/index.tsx
@@ -1,28 +1,11 @@
-import React, { FC, useEffect, useRef } from "react";
+import React, { FC } from "react";
 import { hot } from "react-hot-loader/root";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import { useAuthRedux } from "container/auth";
-import { getTokenToStorage } from "utils/stroage";
 import GlobalStyle from "../styles/GlobalStyle";
 import { Main, Auth } from "container/index";
 
 const App: FC = () => {
-  const didMountRef = useRef(false);
-  const {
-    authReducer: { setToken }
-  } = useAuthRedux();
-
-  useEffect(() => {
-    if (!didMountRef.current) {
-      didMountRef.current = true;
-
-      setToken({
-        accessToken: getTokenToStorage("accessToken") || ""
-      });
-    }
-  });
-
   return (
     <BrowserRouter>
       <GlobalStyle />
