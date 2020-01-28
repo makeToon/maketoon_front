ec9879403fa404b1f86f36646b0fce0bb7ef1971
jjunu <junukim.dev@gmail.com>
2020-01-28 19:17:36 +0900

    [CREATE] header/index.tsx views

---
+++ app/src/components/header/index.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from "react";
+import { Link } from "react-router-dom";
+
+import { useAuthRedux } from "container/auth";
+import { Logo } from "assets/index";
+import * as S from "./style";
+
+const Header: FC = () => {
+  const {
+    authStore: { name, pictureUrl }
+  } = useAuthRedux();
+
+  return (
+    <S.Wrapper>
+      <div>
+        <S.LogoLabel>
+          <Link to="/comics">
+            <img src={Logo} alt="로고" />
+          </Link>
+        </S.LogoLabel>
+        <S.UserLabel>
+          <img src={pictureUrl} alt="유저 프로필" />
+          <span>{name}</span>
+        </S.UserLabel>
+      </div>
+    </S.Wrapper>
+  );
+};
+
+export default Header;
