b4abdf794b142fc173b15c592f70f2e80a593ed4
jjunu <junukim.dev@gmail.com>
2020-01-31 22:59:09 +0900

    [CRAETE] DescriptionArea.tsx

---
+++ app/src/components/main/DescriptionArea.tsx
@@ -0,0 +1,65 @@
+import React, { FC, useState, useCallback, useRef } from "react";
+
+import * as S from "./style";
+
+interface OwnProps {
+  title?: string;
+  description: string[];
+  top?: string;
+  left?: string;
+  right?: string;
+  bottom?: string;
+}
+
+const DescriptionArea: FC<OwnProps> = ({
+  title,
+  description,
+  top,
+  left,
+  right,
+  bottom
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const isOpenHandler = useCallback(() => {
+    if (isOpen) {
+      ref.current.scrollTo({
+        behavior: "smooth",
+        left: 0,
+        top: 0
+      });
+    }
+
+    setIsOpen(!isOpen);
+  }, [isOpen]);
+
+  return (
+    <S.DescriptionArea
+      onClick={isOpenHandler}
+      isOpen={isOpen}
+      top={top}
+      left={left}
+      right={right}
+      bottom={bottom}
+      ref={ref}
+    >
+      {title && <h3>{title}</h3>}
+      {description.map((d, i) => (
+        <p key={i.toString()}>
+          ◈
+          {d.split("/").map((line, index) => {
+            return (
+              <span key={index.toString()}>
+                {line}
+                <br />
+              </span>
+            );
+          })}
+        </p>
+      ))}
+    </S.DescriptionArea>
+  );
+};
+
+export default DescriptionArea;
