09b6cafc70c8bfc463b81325eab7fda8192e5ecb
jjunu <junukim.dev@gmail.com>
2020-02-02 22:49:10 +0900

    [UPDATE] assets/map add eventListener

--- app/src/assets/map/style/index.ts
+++ app/src/assets/map/style/index.ts
@@ -1,6 +1,6 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import { COLORS } from 'src/styles/GlobalStyle';
+import { COLORS } from "src/styles/GlobalStyle";
 
 export const Path = styled.path`
   cursor: pointer;
