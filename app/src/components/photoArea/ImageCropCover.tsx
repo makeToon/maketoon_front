4d5aac0c368f683591ad770633ac5cffea8b0ad1
junukim <junukim.dev@gmail.com>
2020-07-29 13:37:26 +0900

    [UPDATE] fix api

--- app/src/components/photoArea/ImageCropCover.tsx
+++ app/src/components/photoArea/ImageCropCover.tsx
@@ -5,14 +5,23 @@ import ImageCrop from "components/common/imageCrop";
 interface OwnProps {
   setPhoto: (photo: File) => void;
   setCroppedImageUrl: (croppedImageUrl: string) => void;
+  setLocation: ({ width, height }: { width: number; height: number }) => void;
 }
 
-const ImageCropCover: FC<OwnProps> = ({ setCroppedImageUrl, setPhoto }) => {
+const ImageCropCover: FC<OwnProps> = ({
+  setCroppedImageUrl,
+  setPhoto,
+  setLocation,
+}) => {
   return (
     <div>
       <label htmlFor="imageInput">재등록</label>
       <p>※ 이미지를 가장 큰 비율로 자르는 것을 추천드립니다.</p>
-      <ImageCrop setPhoto={setPhoto} setCroppedImageUrl={setCroppedImageUrl} />
+      <ImageCrop
+        setPhoto={setPhoto}
+        setCroppedImageUrl={setCroppedImageUrl}
+        setLocation={setLocation}
+      />
     </div>
   );
 };
