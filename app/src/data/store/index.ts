2dd7329de30090e4ccbbd50379a061c6f632ac58
junukim <junukim.dev@gmail.com>
2020-07-30 14:24:27 +0900

    [UPDATE] saga middleware

--- app/src/data/store/index.ts
+++ app/src/data/store/index.ts
@@ -1,11 +1,17 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import reduxSaga from "redux-saga";
+import { createBrowserHistory } from "history";
 
 import reducer from "../reducers";
 import sagas from "middleware/sagas";
 
-const sagaMiddleWare = reduxSaga();
+const customHistory = createBrowserHistory();
+const sagaMiddleWare = reduxSaga({
+  context: {
+    history: customHistory,
+  },
+});
 
 // redux mapState type
 export type AppState = ReturnType<typeof reducer>;
